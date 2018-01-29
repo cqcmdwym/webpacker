3c9da7cd7382646f9322532ba0546539a5f11c00
chencoo <cookie.chen@autodesk.com>
2018-01-29 13:52:05 +0800

    mulitple modules

--- v1/webpack.config.js
+++ v1/webpack.config.js
@@ -1,14 +1,23 @@
 var path = require('path');
+var webpack = require('webpack');
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
     context: path.resolve('js'),
-    entry:["./utils.js","./app.js"],
+    entry:{
+        about:"./about_page.js",
+        home:"./homt_page.js",
+        contact:"./contact_page.js"
+    },
     output:{
         path: path.resolve('build/js/'), //source code path
         publicPath:'/public/assets/js/', //index.html logic path and mapping to source code path
-        filename:"bundle.js"//build.js under build/js directory
+        filename:"[name].js"//name mapping to the key(about,home,contact)
     },
-
+    plugins:[
+        commonsPlugin
+    ],
     devServer:{
         contentBase:'public'
     }, // bundle file isn't actually produced and put on the disk, it's just served up virtually by the web server
@@ -25,6 +34,11 @@ module.exports = {
                 test: /\.es6$/,
                 exclude:/node_modules/,
                 loader:"babel-loader"
+            },
+            {
+                test:/\.css$/,
+                exclue:/node_modules/,
+                loader:"style-loader!css-loader" //run through from css-loader first and then run the result through the style-loader
             }
         ]
     },
